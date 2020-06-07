a1cebee543dd640a55f52f41a547f1cc2ddce263
Gem Xli <x.li.gem@gmail.com>
2020-06-07 21:55:19 +0800

    chore: upgrade dependencies

--- bin/config.js
+++ bin/config.js
@@ -1,43 +1,49 @@
 module.exports = {
-  registries: [{
-      name: 'npm',
-      home: 'https://www.npmjs.org',
-      registry: 'https://registry.npmjs.org/'
+  registries: [
+    {
+      name: "npm",
+      home: "https://www.npmjs.org",
+      registry: "https://registry.npmjs.org/",
     },
     {
-      name: 'yarn',
-      home: 'https://yarnpkg.com',
-      registry: 'https://registry.yarnpkg.com/'
+      name: "yarn",
+      home: "https://yarnpkg.com",
+      registry: "https://registry.yarnpkg.com/",
     },
     {
-      name: 'cnpm',
-      home: 'http://cnpmjs.org',
-      registry: 'http://r.cnpmjs.org/'
+      name: "cnpm",
+      home: "http://cnpmjs.org",
+      registry: "http://r.cnpmjs.org/",
     },
     {
-      name: 'taobao',
-      home: 'https://npm.taobao.org',
-      registry: 'https://registry.npm.taobao.org/'
+      name: "taobao",
+      home: "https://npm.taobao.org",
+      registry: "https://registry.npm.taobao.org/",
     },
     {
-      name: 'nj',
-      home: 'https://www.nodejitsu.com',
-      registry: 'https://registry.nodejitsu.com/'
+      name: "nj",
+      home: "https://www.nodejitsu.com",
+      registry: "https://registry.nodejitsu.com/",
     },
     {
-      name: 'rednpm',
-      home: 'http://npm.mirror.cqupt.edu.cn/',
-      registry: 'http://registry.mirror.cqupt.edu.cn/'
+      name: "rednpm",
+      home: "http://npm.mirror.cqupt.edu.cn/",
+      registry: "http://registry.mirror.cqupt.edu.cn/",
     },
     {
-      name: 'npmMirror',
-      home: 'https://skimdb.npmjs.com/',
-      registry: 'https://skimdb.npmjs.com/registry/'
+      name: "npmMirror",
+      home: "https://skimdb.npmjs.com/",
+      registry: "https://skimdb.npmjs.com/registry/",
     },
     {
-      name: 'edunpm',
-      home: 'http://www.enpmjs.org',
-      registry: 'http://registry.enpmjs.org/'
-    }
-  ]
+      name: "edunpm",
+      home: "http://www.enpmjs.org",
+      registry: "http://registry.enpmjs.org/",
+    },
+    {
+      name: "github",
+      home: "https://github.com/features/packages",
+      registry: "https://npm.pkg.github.com",
+    },
+  ],
 };
